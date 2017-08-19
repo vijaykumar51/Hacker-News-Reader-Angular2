10d6a9ef1e250534f8c52935f001c38699e130c0
vijaykumar51 <vijay.kumar.em51@gmail.com>
2017-08-19 18:02:14 +0530

    item component css changes, routing changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,20 @@ import { RouterModule, Routes } from "@angular/router";
 import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
-import { AskComponent, HeaderComponent, ItemComponent, JobComponent,
-	NewsComponent, ShowComponent, FooterComponent } from "./components/component-collection";
+import {
+	AskComponent, HeaderComponent, ItemComponent, JobComponent,
+	NewsComponent, ShowComponent, FooterComponent
+} from "./components/component-collection";
 import { ApiService } from "./services/api.service";
 
+const feedRoutes = [{
+	component: NewsComponent,
+	path: ":page"
+}];
+
 const appRoutes: Routes = [
-	{ path: "", redirectTo: "news", pathMatch: "full" },
-	{ path: "news", component: NewsComponent },
+	{ path: "", redirectTo: "news/1", pathMatch: "full" },
+	{ path: "news", children: feedRoutes },
 	{ path: "show", component: ShowComponent },
 	{ path: "ask", component: AskComponent },
 	{ path: "jobs", component: JobComponent }
@@ -20,7 +27,7 @@ const appRoutes: Routes = [
 	bootstrap: [AppComponent],
 	declarations: [AppComponent, HeaderComponent, ItemComponent, NewsComponent,
 		FooterComponent, ShowComponent, AskComponent, JobComponent],
-	imports: [BrowserModule, HttpModule, RouterModule.forRoot(appRoutes, { useHash: true })],
+	imports: [BrowserModule, HttpModule, RouterModule.forRoot(appRoutes, { useHash: false })],
 	providers: [ApiService],
 })
 export class AppModule {
--- src/components/news-component/news.component.ts
+++ src/components/news-component/news.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Http } from "@angular/http";
+import { ActivatedRoute } from "@angular/router";
 import { ApiService } from "../../services/api.service";
 import { NewsFeed } from "../../models/model-collection";
 
@@ -9,13 +10,18 @@ import { NewsFeed } from "../../models/model-collection";
 	templateUrl: "./news.component.html",
 })
 export class NewsComponent implements OnInit {
+	public pageNumber: number;
 
 	public newsList: NewsFeed[] = [];
 
-	constructor(private apiService: ApiService) { }
+	constructor(private apiService: ApiService, private route: ActivatedRoute) {
+		this.route.params.subscribe(params => {
+			this.pageNumber = params["page"];
+		});
+	}
 
 	public ngOnInit() {
-		this.apiService.newsHttpRequest(1).subscribe(response => {
+		this.apiService.newsHttpRequest(this.pageNumber).subscribe(response => {
 			console.log(response);
 			this.newsList = response;
 		});
